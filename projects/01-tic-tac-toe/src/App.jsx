c4c3b8e3c895348a006ad49b5793ff1016dcb125
brayanavila14 <brayandavidavilaorozco@gmail.com>
2024-04-16 22:50:47 -0500

    Refactor Tic Tac Toe game logic and state management

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,95 +1,121 @@
-import { useState } from 'react'
-import confetti from 'canvas-confetti'
-import Square from './components/Square'
-import Modal from './components/ModalWinner'
-import './App.css'
+import { useState } from "react";
+import confetti from "canvas-confetti";
+import Square from "./components/Square";
+import Modal from "./components/ModalWinner";
+import "./App.css";
 
 const App = () => {
-  const [history, setHistory] = useState(JSON.parse(localStorage.getItem('history')) || [Array(9).fill(null)])
-  const [board, setBoard] = useState(JSON.parse(localStorage.getItem('board')) || Array(9).fill(null))
-  const [isOpenModal, setIsOpenModal] = useState(JSON.parse(localStorage.getItem('isOpenModal')))
-  const [isXNext, setIsXNext] = useState(JSON.parse(localStorage.getItem('isXNext')))
-  const turn = isXNext ? 'X' : 'O'
+  const [history, setHistory] = useState(
+    JSON.parse(localStorage.getItem("history")) || [Array(9).fill(null)]
+  );
+  const [board, setBoard] = useState(
+    JSON.parse(localStorage.getItem("board")) || Array(9).fill(null)
+  );
+  const [isOpenModal, setIsOpenModal] = useState(
+    JSON.parse(localStorage.getItem("isOpenModal"))
+  );
+  const [isXNext, setIsXNext] = useState(
+    JSON.parse(localStorage.getItem("isXNext"))
+  );
+  const turn = isXNext ? "X" : "O";
 
   const handleClick = (index) => {
-    if (calculateWinner(board) || board[index]) return
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setHistory([...history, newBoard])
-    setBoard(newBoard)
-    setIsXNext(!isXNext)
-    localStorage.setItem('history', JSON.stringify([...history, newBoard]))
-    localStorage.setItem('isXNext', JSON.stringify(!isXNext))
-    localStorage.setItem('board', JSON.stringify(newBoard))
-    localStorage.setItem('isOpenModal', JSON.stringify(false))
+    if (calculateWinner(board) || board[index]) return;
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setHistory([...history, newBoard]);
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+    localStorage.setItem("history", JSON.stringify([...history, newBoard]));
+    localStorage.setItem("isXNext", JSON.stringify(!isXNext));
+    localStorage.setItem("board", JSON.stringify(newBoard));
+    localStorage.setItem("isOpenModal", JSON.stringify(false));
     if (calculateWinner(newBoard)) {
-      confetti()
-      setIsOpenModal(true)
-      localStorage.setItem('isOpenModal', JSON.stringify(true))
+      confetti();
+      setIsOpenModal(true);
+      localStorage.setItem("isOpenModal", JSON.stringify(true));
     }
     if (newBoard.every((square) => square !== null)) {
-      setIsOpenModal(true)
-      localStorage.setItem('isOpenModal', JSON.stringify(true))
+      setIsOpenModal(true);
+      localStorage.setItem("isOpenModal", JSON.stringify(true));
     }
-  }
+  };
   const calculateWinner = (board) => {
     const lines = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6],
-    ]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
     for (const line of lines) {
-      const [a, b, c] = line
+      const [a, b, c] = line;
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return board[a]
+        return board[a];
       }
     }
-    return null
-  }
+    return null;
+  };
   const resetGame = () => {
-    setHistory([Array(9).fill(null)])
-    setBoard(Array(9).fill(null))
-    setIsXNext(true)
-    setIsOpenModal(false)
-    localStorage.setItem('history', JSON.stringify([Array(9).fill(null)]))
-    localStorage.setItem('isXNext', JSON.stringify(true))
-    localStorage.setItem('board', JSON.stringify(Array(9).fill(null)))
-    localStorage.setItem('isOpenModal', JSON.stringify(false))
-  }  
+    setHistory([Array(9).fill(null)]);
+    setBoard(Array(9).fill(null));
+    setIsXNext(true);
+    setIsOpenModal(false);
+    localStorage.setItem("history", JSON.stringify([Array(9).fill(null)]));
+    localStorage.setItem("isXNext", JSON.stringify(true));
+    localStorage.setItem("board", JSON.stringify(Array(9).fill(null)));
+    localStorage.setItem("isOpenModal", JSON.stringify(false));
+  };
   return (
     <>
       <main className="board">
         <h1>Tic Tac Toe</h1>
-        <section className='game'>
+        <section className="game">
           {board.map((_, index) => (
-            <Square key={index} value={board[index]} onClick={() => handleClick(index)} />
+            <Square
+              key={index}
+              value={board[index]}
+              onClick={() => handleClick(index)}
+            />
           ))}
         </section>
-        <section className='turn'>
+        <section className="turn">
           <h2>Next player: </h2>
-          <Square value='X' isSelected={turn === 'X'} />
-          <Square value='O' isSelected={turn === 'O'} />
+          <Square value="X" isSelected={turn === "X"} />
+          <Square value="O" isSelected={turn === "O"} />
         </section>
-        <Modal isOpenModal={isOpenModal} board={board} resetGame={resetGame} calculateWinner={calculateWinner} />
+        <Modal
+          isOpenModal={isOpenModal}
+          board={board}
+          resetGame={resetGame}
+          calculateWinner={calculateWinner}
+        />
       </main>
-      <aside className='history'>
+      <aside className="history">
         <h1>History</h1>
-        {
-          history.map((_, index) => (
-            <button key={index} onClick={() => {
-              setBoard(history[index])
-              setIsXNext(index % 2 === 0)
-              setHistory(history.slice(0, index + 1))
-              localStorage.setItem('history', JSON.stringify(history.slice(0, index + 1)))
-              localStorage.setItem('isXNext', JSON.stringify(index % 2 === 0))
-              localStorage.setItem('board', JSON.stringify(history[index]))
-            }}>
-            {index === 0 ? 'Start' : `Move #${index}`}
-            </button>
-          ))
-        }
+        {history.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              setBoard(history[index]);
+              setIsXNext(index % 2 === 0);
+              setHistory(history.slice(0, index + 1));
+              localStorage.setItem(
+                "history",
+                JSON.stringify(history.slice(0, index + 1))
+              );
+              localStorage.setItem("isXNext", JSON.stringify(index % 2 === 0));
+              localStorage.setItem("board", JSON.stringify(history[index]));
+            }}
+          >
+            {index === 0 ? "Start" : `Move #${index}`}
+          </button>
+        ))}
       </aside>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
