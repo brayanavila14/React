6b753c4e523617f0e2af97cbd8ed2446094b6d20
brayanavila14 <brayandavidavilaorozco@gmail.com>
2024-06-26 21:42:32 -0500

    update

--- projects/03-ecommerce/src/App.jsx
+++ projects/03-ecommerce/src/App.jsx
@@ -1,13 +1,18 @@
-import Products from './components/Products/Products'
-import Filters from './components/Filters/Filters'
-
+import { products as initialProducts } from "./mocks/products"
+import Products from "./components/Products/Products"
+import Filters from "./components/Filters/Filters"
+import Carts from "./components/Carts/Carts"
+import { useState } from "react"
 function App() {
+  const [ allProducts, setAllProducts ] = useState(initialProducts)
+  const [ carts, setCarts ] = useState([])
   return (
     <>  
       <div className="App">
-        <h1>Productos</h1>
-        <Filters />
-        <Products />
+        <h1>e-commerce practice</h1>
+        <Filters setFilteredProducts={setAllProducts} />
+        <Carts carts={carts} setCarts={setCarts} />
+        <Products products={allProducts} carts={carts} setCarts ={setCarts} />
       </div>
     </>
   )
--- projects/03-ecommerce/src/components/Products/Products.jsx
+++ projects/03-ecommerce/src/components/Products/Products.jsx
@@ -1,39 +1,53 @@
-import useApi from "../../hooks/useApi"
-import { AddToCartIcon, RemoveFromCartIcon  } from "../Icons"
-import useCart from "../../hooks/useCart"
 import './Products.css'
-function Products() {
-    const data = useApi('https://fakestoreapi.com/products')
-    const { cart, addToCart, removeFromCart } = useCart()
-    const isAdded = product => {
-        return cart.some(item => item.id === product.id)
+import { AddToCartIcon } from '../Icons'
+import PropTypes from "prop-types";
+
+export default function Products({ products, setCarts, carts }) {
+    const handleAddToCart = (product) => {
+    const productInCartIndex = carts.findIndex(item => item.id === product.id)
+    if (productInCartIndex !== -1) {
+        setCarts(carts.map(item =>
+            item.id === product.id
+                ? { ...item, quantity: typeof item.quantity === 'number' ? item.quantity + 1 : 1 }
+                : item
+        ))
+    } else {
+        setCarts([...carts, { ...product, quantity: 1 }])
     }
-    return (
-        <main className="products">
-            <ul>
-                {data.slice(0, 10).map((product) => (
-                    <li key={data.id}>
-                        <h3>{data.title}</h3>
-                        <img src={product.image} alt={product.title} />
-                        <span>{product.category}</span>
-                        <span>${product.price}</span>
-                    </li>
-                ))}
-                {
-                    <button
-                    style={{ backgroundColor: isAdded ? 'red' : 'green'}}
-                    onClick={
-                        isAdded ? 
-                        () => removeFromCart(data) 
-                        : 
-                        () => addToCart(data)
-                    }
-                    >
-                    {isAdded ? <RemoveFromCartIcon /> : <AddToCartIcon />}  
-                    </button>
-                }
-            </ul>
-        </main>
-    )
 }
-export default Products
+    return (
+        <>
+            <h3>Productos</h3>
+            <div className="products">
+                <ul>
+                    {
+                        products.length === 0 && <h1 className='notfound'>No hay Productos</h1>
+                    }
+                    {
+                    products.map((product) => (
+                        <li className='product' key={product.id}>
+                        <img src={product.thumbnail} alt={product.title} />
+                        <div>
+                            <h3>{product.title}</h3>
+                            <p>{product.category}</p>
+                            <span>${product.price}</span>
+                        </div>
+                        <div>
+                            <button
+                                onClick={() => handleAddToCart(product)}
+                            >
+                                <AddToCartIcon />
+                            </button>
+                        </div>
+                    </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+}
+Products.propTypes = {
+    products: PropTypes.array.isRequired,
+    setCarts: PropTypes.func.isRequired,
+    carts: PropTypes.array.isRequired,
+};
